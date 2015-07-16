0749d17b39c18e90a789b36efa072e8e1cee6285
wenhung yang <fyonyung@gmail.com>
2015-07-17 07:41:54 +0800

    remove do not need usb id

--- task05/hello2.c
+++ task05/hello2.c
@@ -3,22 +3,12 @@
 #include <linux/usb.h>
 #include <linux/hid.h>
 
-/* Define these values to match your devices */
-#define USB_SKEL_VENDOR_ID	0xFFF0
-#define USB_SKEL_PRODUCT_ID	0xFFF0
-
 /* table of devices that work with this driver */
 static const struct usb_device_id my_usb_table[] = {
-	/* special vendor id , product id device */
-	{ USB_DEVICE(USB_SKEL_VENDOR_ID, USB_SKEL_PRODUCT_ID) },
 	/* any USB keyboard */
 	{ USB_INTERFACE_INFO(USB_INTERFACE_CLASS_HID,
 				USB_INTERFACE_SUBCLASS_BOOT,
 				USB_INTERFACE_PROTOCOL_KEYBOARD) },
-	/* any USB Mouse */
-	{ USB_INTERFACE_INFO(USB_INTERFACE_CLASS_HID,
-				USB_INTERFACE_SUBCLASS_BOOT,
-				USB_INTERFACE_PROTOCOL_MOUSE) },
 	{ }					/* Terminating entry */
 };
 MODULE_DEVICE_TABLE(usb, my_usb_table);
